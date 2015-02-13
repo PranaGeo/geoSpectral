36ae743f240b8ade80b75d3dd3fbf47e74d5214d
Servet Ahmet Cizmeli <sa.acizmeli@usherbrooke.ca>
2015-02-13 10:51:02 +0200

    Including Spectral-package documentation in Spectral.R

---
+++ R/Spectral.R
@@ -0,0 +1,14 @@
+#' Manipulation of spectral (radiometric) data
+#'
+#' \pkg{Spectral} is an R package providing a new data type for R that stores
+#'  spectral,temporal and spatial attributes of measurement data as well as methods for 
+#' accessing and manipulating the spectral (and non-spectral) data.
+#' with code.
+#'
+#' The only function you're likely to need from \pkg{Spectral} is
+#' \code{\link{roxygenize}}. Otherwise refer to the vignettes to see
+#' how to format the documentation.
+#'
+#' @docType package
+#' @name Spectral
+NULL
