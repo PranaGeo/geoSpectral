18585ba302a6dc70b4529da1f6325c8eb878cc57
baris yilmaz <baris@pranageo.com>
2016-08-04 13:46:28 +0300

    created new function spc.example_spectra()

--- R/Spectra-Methods.R
+++ R/Spectra-Methods.R
@@ -1765,3 +1765,20 @@ setMethod("spc.plot.depth", signature="Spectra", function (object,X,maxSp=10,lab
   }
 })
 
+spc.example_spectra = function(){
+  fnm = file.path(base::system.file(package = "geoSpectral"),"test_data","particulate_absorption.csv.gz")
+  abs = read.table(fnm,sep=",",header=TRUE)
+  abs$STATION=factor(abs$STATION)
+  abs[1:2,1:17] #Display only the first 2 rows and first 17 columns if the data frame
+  lbd = as.numeric(gsub("X","",colnames(abs)[14:514]))
+  Units="1/m"
+  colnames(abs)= gsub("X",paste("anap","_",sep=""), colnames(abs))
+  colnames(abs)= gsub("PRES","DEPTH", colnames(abs))
+  abs = abs[,c(14:514,1:13)] #Rearrange so that Spectra columns come first
+  tz<-strsplit(as.character(abs$TIME)," ")[[1]][[3]] #Extract the timezone
+  abs$TIME = as.POSIXct(as.character(abs$TIME),tz=tz) #Compute the time
+  
+  #Space and time columns are automatically found in the column names of inDF
+  myS<-Spectra(abs,Wavelengths=lbd,Units=Units,ShortName="a_nap")
+  myS
+}
