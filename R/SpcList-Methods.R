f8ac8161ea17cf78cbbc18503b40863332342bcd
Ahmet Cizmeli <ahmet@pranageo.com>
2017-02-07 16:11:09 +0300

    Defect 8669 fixed. Now spc.list() returns a list object if the result contents are not all Spectra objects.

--- R/SpcList-Methods.R
+++ R/SpcList-Methods.R
@@ -635,15 +635,22 @@ setMethod("sort", signature="SpcList", definition= function (x, decreasing = FAL
 #' @return FUN  function to be applied to each element of X
 #' @examples  
 #' sp=spc.example_spectra()
-#' BL=spc.data2header(sp,"CAST")
+#' BL=spc.makeSpcList(sp,"CAST")
 #' spc.lapply(BL,function(x) {nrow(x)})
+#' #Perform arithmetic operations on all Spectra elements
+#' spc.lapply(BL,function(x) {x^2+1})
 setGeneric (name= "spc.lapply",
 		def=function(X, FUN,...){standardGeneric("spc.lapply")})
 setMethod("spc.lapply", signature="SpcList", definition= function (X, FUN, ...) {
 			by = X@by
+			by_names <- names(X)
 			X = lapply(as(X,"list"),FUN,...)
-			X = as(X, "SpcList")
-			X@by = by
+			if (all(sapply(X, class)=="Spectra")) {
+			  X = as(X, "SpcList")
+			  X@by = by
+			} else {
+			  names(X) <- by_names
+			}
 			validObject(X)
 			return(X)
 		})
