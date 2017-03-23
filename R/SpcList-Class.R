abc48fa048213db6c8fcce9d77d3f8675f0cbe5c
Khalil Haji <khalilalihaji@gmail.com>
2017-03-23 17:18:14 +0300

    leftover merge fix

--- R/SpcList-Class.R
+++ R/SpcList-Class.R
@@ -5,7 +5,8 @@
 #' @description Definition for \code{SpcList}, a class to store multiple \code{Spectra}
 #' objects inside a list-like object. See the help of the constructor function \code{\link{SpcList}}.
 #' @slot .Data list, Inherited R list object
-#' @slot by character, Determines the header field in the Spectra objects.
+#' @slot by character, Determines the header field in the Spectra objects
+
 #' within the SpcList that describes how they are different one from the other.
 #' @export
 setClass("SpcList",contains="list",
